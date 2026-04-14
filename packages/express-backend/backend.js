25c53d91e37e9ec7a5e518e51cb124f178489e34
meganrobin <156228985+meganrobin@users.noreply.github.com>
2026-04-13 18:51:45 -0700

    Add user retrieval by ID endpoint to backend

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -33,6 +33,9 @@ const users = {
   ]
 };
 
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -59,5 +62,15 @@ app.get("/users", (req, res) => {
     res.send(result);
   } else {
     res.send(users);
+  }
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
   }
 });
