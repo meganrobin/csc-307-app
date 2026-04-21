da25221027d5ea6f04bc9fb4c4f4054e1b8c3d35
meganrobin <156228985+meganrobin@users.noreply.github.com>
2026-04-20 19:52:24 -0700

    Enable frontend and backend to be able to talk to eachother

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express from "express";
 
 const app = express();
@@ -53,6 +54,7 @@ const addUser = (user) => {
     return user;
 };
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
